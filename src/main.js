1f733a12dc8d3da0f02ea106edb97b3db109a0f5
yousaisai <yousaisai@qutoutiao.net>
2021-06-09 10:15:20 +0800

    flexible换成本地的

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'amfe-flexible'
-
+import './utils/flexible'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
